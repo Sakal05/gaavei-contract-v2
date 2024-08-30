96b4a956a197b4a87f6e691e99077c018a877158
Sakal05 <samnang.sakal@gmail.com>
2024-08-30 21:53:56 +0700

    ok

---
+++ script/deploy.s.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {Upgrades, Option} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+
+contract DeployDefender is Script {}
